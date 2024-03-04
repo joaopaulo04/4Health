18cd3bb6bf8110fb77549d920e1b639d4e454e7c
ArthurDeFaria <arthur.faria@aluno.ifsp.edu.br>
2024-03-03 22:52:19 -0300

    Flet implementado

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Olá Arthur, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flet import *
 
 
-# Press the green button in the gutter to run the script.
+def main(page: Page):
+    pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app(target=main)
