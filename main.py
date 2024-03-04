57ecd241fb401bd84fa130b47df19677b38081b6
João Paulo Costa <joaopaulo.pc27@gmail.com>
2024-03-03 22:43:38 -0300

    Arthur de Faria teste.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Arthur de Faria, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Olá Arthur, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
