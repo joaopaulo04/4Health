dd9377d80325ace10ee123515ba87bb97d43e46c
Leo L <leonardolimace19@gmail.com>
2024-03-11 04:29:00 -0300

    Teste grupal

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flet import *
 
 def main(page: Page):
     page.add(Text("Hello World!"))
+    page.add(Text("Size 10", size=10))
     page.title = '4Health'
-    pass
+    page.theme_mode = ThemeMode.LIGHT
+    page.window_width = 30
+    page.window_height = 100
+    page.update()
 
 
 if __name__ == '__main__':
