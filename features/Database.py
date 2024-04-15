020b4ecf24972fe70f96a3251de104500d2ac8f5
João Paulo Costa <joaopaulo.pc27@gmail.com>
2024-04-15 13:07:54 -0300

    Tela de cadastro e mudanças no Database

--- features/Database.py
+++ features/Database.py
@@ -3,14 +3,15 @@ import os
 
 DATABASE_NAME = "banco.db"
 
+
 class DataMethods():
-    #Metodo para criar conexão
+    # Metodo para criar conexão
     @classmethod
     def create_connection(cls):
         connection = connect(f'{DATABASE_NAME}')
         return connection
-        
-    #Metodo para executar querys
+
+    # Metodo para executar querys
     @classmethod
     def execute_query(cls, query):
         connection = cls.create_connection()
@@ -18,8 +19,8 @@ class DataMethods():
         cursor.execute(query)
         connection.commit()
         connection.close()
-        
-    #Metodo para identificar se o banco ja existe ou nao
+
+    # Metodo para identificar se o banco ja existe ou nao
     @classmethod
     def initialize(cls):
         print("Inicializando banco")
@@ -32,6 +33,7 @@ class DataMethods():
                               f'id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,'
                               f'nome TEXT NOT NULL,'
                               f'email TEXT UNIQUE NOT NULL,'
+                              f'cpf INTEGER UNIQUE NOT NULL,'
                               f'senha TEXT NOT NULL,'
                               f'data_nascimento TEXT NOT NULL,'
                               f'sexo TEXT NOT NULL,'
@@ -63,9 +65,8 @@ class DataMethods():
                               f'desc_exame TEXT,'
                               f'hora_exame TEXT NOT NULL,'
                               f'data_exame TEXT NOT NULL)')
-            DataMethods.add_users("Arthur", "arthur.faria@gmail.com", "arthur123", "24/08/2004", "M", 19995128382, "A+",1.70, 86.4)
 
-    #Metodo para ver valores do banco
+    # Metodo para ver valores do banco
     @classmethod
     def show_users(cls):
         connection = cls.create_connection()
@@ -102,8 +103,7 @@ class DataMethods():
         connection.close()
         return remedio
 
-
-    #Metodo para verificar login
+    # Metodo para verificar login
     @classmethod
     def verify_login(cls, email: str, senha: str) -> bool:
         connection = cls.create_connection()
@@ -112,7 +112,7 @@ class DataMethods():
         data = cursor.fetchall()
         for user in data:
             if user[2] == email:
-                if user[3] == senha:
+                if user[4] == senha:
                     return True
                 else:
                     return False
@@ -121,24 +121,46 @@ class DataMethods():
         connection.close()
         return False
 
-    #Metodos para adicionar/remover/editar usuarios(table)
     @classmethod
-    def add_users(cls, nome, email, senha, data_nascimento, sexo, telefone, tipo_sanguineo, altura, peso):
+    def get_user_by_email(cls, email):
         connection = cls.create_connection()
         cursor = connection.cursor()
-        cursor.execute(f"INSERT INTO usuarios(nome, email, senha, data_nascimento, sexo, telefone , tipo_sanguineo, altura, peso) VALUES ("
-                       f"'{nome}',"
-                       f"'{email}',"
-                       f"'{senha}',"
-                       f"'{data_nascimento}',"
-                       f"'{sexo}',"
-                       f"{telefone},"
-                       f"'{tipo_sanguineo}',"
-                       f"{altura},"
-                       f"{peso}"
-                       f")")
+        cursor.execute(f"SELECT * FROM usuarios WHERE email='{email}'")
+        user = cursor.fetchone()
+        connection.close()
+        return user
+
+    @classmethod
+    def get_user_by_cpf(cls, cpf):
+        connection = cls.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM usuarios WHERE cpf={cpf}")
+        user = cursor.fetchone()
+        connection.close()
+        return user
+
+    # Metodos para adicionar/remover/editar usuarios(table)
+    @classmethod
+    def add_users(cls, nome, email, cpf, senha, data_nascimento, sexo, telefone, tipo_sanguineo, altura, peso):
+        connection = cls.create_connection()
+        cursor = connection.cursor()
+        cursor.execute(
+            f"INSERT INTO usuarios(nome, email, cpf, senha, data_nascimento, sexo, telefone , tipo_sanguineo, altura, peso) VALUES ("
+            f"'{nome}',"
+            f"'{email}',"
+            f"{cpf},"
+            f"'{senha}',"
+            f"'{data_nascimento}',"
+            f"'{sexo}',"
+            f"{telefone},"
+            f"'{tipo_sanguineo}',"
+            f"{altura},"
+            f"{peso}"
+            f")")
         connection.commit()
         connection.close()
+        return True
+
     @classmethod
     def remove_users(cls, id_usuario):
         cls.execute_query(f"DELETE FROM usuarios WHERE id_usuario = {id_usuario}")
@@ -151,44 +173,41 @@ class DataMethods():
                           f"peso = {peso} "
                           f"WHERE id_usuario = {id_usuario};")
 
-    @classmethod
-    def edit_user_password(cls, id_usuario, senha):
-        cls.execute_query(f"UPDATE usuarios SET senha = '{senha}'"
-                          f"WHERE id_usuario = {id_usuario};")
-        
-    #Metodos para adicionar/remover/editar remedios
+    # Metodos para adicionar/remover/editar remedios
     @classmethod
     def add_remedio(cls, id_usuario, nome_remedio, desc_remedio, intervalo_uso, primeiro_uso):
-        cls.execute_query(f"INSERT INTO remedio(id_usuario, nome_remedio, desc_remedio, intervalo_uso, primeiro_uso) VALUES("
-                       f"{id_usuario},"
-                       f"'{nome_remedio}',"
-                       f"'{desc_remedio}',"
-                       f"'{intervalo_uso}',"
-                       f"'{primeiro_uso}'"
-                       f")")
+        cls.execute_query(
+            f"INSERT INTO remedio(id_usuario, nome_remedio, desc_remedio, intervalo_uso, primeiro_uso) VALUES("
+            f"{id_usuario},"
+            f"'{nome_remedio}',"
+            f"'{desc_remedio}',"
+            f"'{intervalo_uso}',"
+            f"'{primeiro_uso}'"
+            f")")
 
     @classmethod
     def remove_remedio(cls, id_remedio):
         cls.execute_query(f"DELETE FROM remedio WHERE id_remedio = {id_remedio};")
 
     @classmethod
-    def edit_remedio(cls,id_remedio, nome_remedio, desc_remedio, intervalo_uso, primeiro_uso):
+    def edit_remedio(cls, id_remedio, nome_remedio, desc_remedio, intervalo_uso, primeiro_uso):
         cls.execute_query(f"UPDATE remedio SET nome_remedio = '{nome_remedio}',"
                           f"desc_remedio = '{desc_remedio}',"
                           f"intervalo_uso = '{intervalo_uso}',"
                           f"primeiro_uso = '{primeiro_uso}'"
                           f"WHERE id_remedio = {id_remedio};")
-        
-    #Metodos para adicionar/remover/editar consultas
+
+    # Metodos para adicionar/remover/editar consultas
     @classmethod
     def add_consulta(cls, id_usuario, nome_consulta, desc_consulta, hora_consulta, data_consulta):
-        cls.execute_query(f"INSERT INTO consulta (id_usuario, nome_consulta, desc_consulta, hora_consulta, data_consulta) VALUES ("
-                       f"{id_usuario},"
-                       f"'{nome_consulta}',"
-                       f"'{desc_consulta}',"
-                       f"'{hora_consulta}',"
-                       f"'{data_consulta}'"
-                       f")")
+        cls.execute_query(
+            f"INSERT INTO consulta (id_usuario, nome_consulta, desc_consulta, hora_consulta, data_consulta) VALUES ("
+            f"{id_usuario},"
+            f"'{nome_consulta}',"
+            f"'{desc_consulta}',"
+            f"'{hora_consulta}',"
+            f"'{data_consulta}'"
+            f")")
 
     @classmethod
     def remove_consulta(cls, id_consulta):
@@ -201,24 +220,24 @@ class DataMethods():
                           f"data_consulta = '{data_consulta}',"
                           f"hora_consulta ='{hora_consulta}'"
                           f"WHERE id_consulta = {id_consulta};")
-        
-    #Metodos para adicionar/remover/editar exames
+
+    # Metodos para adicionar/remover/editar exames
     @classmethod
     def add_exame(cls, id_usuario, nome_exame, desc_exame, data_exame, hora_exame):
         cls.execute_query(f"INSERT INTO exame(id_usuario, nome_exame, desc_exame, data_exame, hora_exame) VALUES("
-                       f"{id_usuario},"
-                       f"'{nome_exame}',"
-                       f"'{desc_exame}',"
-                       f"'{data_exame}',"
-                       f"'{hora_exame}'"
-                       f")")
+                          f"{id_usuario},"
+                          f"'{nome_exame}',"
+                          f"'{desc_exame}',"
+                          f"'{data_exame}',"
+                          f"'{hora_exame}'"
+                          f")")
 
     @classmethod
     def remove_exame(cls, id_exame):
         cls.execute_query(f"DELETE FROM exame WHERE id_exame = {id_exame};")
 
     @classmethod
-    def edit_exame(cls,id_exame, nome_exame, desc_exame, data_exame, hora_exame):
+    def edit_exame(cls, id_exame, nome_exame, desc_exame, data_exame, hora_exame):
         cls.execute_query(f"UPDATE exame SET nome_exame = '{nome_exame}',"
                           f"desc_exame = '{desc_exame}',"
                           f"data_exame = '{data_exame}',"
--- views/SignIn.py
+++ views/SignIn.py
@@ -1,6 +1,265 @@
 from flet import *
+from datetime import datetime
 from features.Database import DataMethods
 
+
+def validate_cpf(cpf):
+    # Remove qualquer caractere que não seja dígito
+    cpf = ''.join(filter(str.isdigit, cpf))
+
+    # Verifica se o CPF tem 11 dígitos
+    if len(cpf) != 11:
+        return False
+
+    # Verifica se todos os dígitos são iguais
+    if cpf == cpf[0] * 11:
+        return False
+
+    # Calcula o primeiro dígito verificador
+    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
+    resto = soma % 11
+    if resto < 2:
+        digito_verificador_1 = 0
+    else:
+        digito_verificador_1 = 11 - resto
+
+    # Verifica se o primeiro dígito verificador está correto
+    if int(cpf[9]) != digito_verificador_1:
+        return False
+
+    # Calcula o segundo dígito verificador
+    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
+    resto = soma % 11
+    if resto < 2:
+        digito_verificador_2 = 0
+    else:
+        digito_verificador_2 = 11 - resto
+
+    # Verifica se o segundo dígito verificador está correto
+    if int(cpf[10]) != digito_verificador_2:
+        return False
+
+    return True
+
+
 def signin(page):
-    content = Text('Sign in ')
+
+    def close_dialog(e):
+        page.dialog.open = False
+        page.update()
+
+    def send_register(e):
+        valid = True
+        error_messages = []
+
+        #Validação do nome
+        name = name_textfield.value.strip() # Remove espaços em branco
+        if len(name) < 3 or len(name) > 50:
+            valid = False
+            error_messages.append("Nome deve ter entre 3 e 50 caracteres.")
+
+        #Validação de email
+        email = email_textfield.value.strip()
+        if not email_textfield.value.strip():
+            valid = False
+            error_messages.append("Email é obrigatório")
+        else:
+            # Verificação se o email já está em uso
+            existing_email = DataMethods.get_user_by_email(email)
+            if existing_email:
+                valid = False
+                error_messages.append("Este email já está em uso")
+
+        #Validação de senha
+        password = password_textfield.value
+        if len(password) < 8:
+            valid = False
+            error_messages.append("Sua senha deve ter no mínimo 8 caracteres")
+        if not any(char.isalnum() for char in password):
+            valid = False
+            error_messages.append("Sua senha deve conter letras e números")
+
+        #Validação de confirmação de senha
+        if password != confirm_password_textfield.value:
+            valid = False
+            error_messages.append("As senhas não batem.")
+
+        #Validação de altura
+        try:
+            height = float(height_textfield.value)
+            if height <= 0:
+                valid = False
+                error_messages.append("Altura deve ser maior que zero")
+        except ValueError:
+            valid = False
+            error_messages.append("Altura inválida (apenas números).")
+
+        #Validação de peso
+        try:
+            weight = float(weight_textfield.value)
+            if weight <= 0:
+                valid = False
+                error_messages.append("Peso deve ser maior que zero")
+        except ValueError:
+            valid = False
+            error_messages.append("Peso inválido (apenas números).")
+
+        # Validação do tipo sanguíneo
+        blood = blood_type_dropdown.value
+        if not blood:
+            valid = False
+            error_messages.append("Por favor, selecione um tipo sanguíneo.")
+
+        #Validação de CPF
+        cpf = cpf_textfield.value.strip()
+
+        if not cpf.isdigit() or len(cpf) != 11:
+            valid = False
+            error_messages.append("CPF inválido (deve conter apenas 11 numeros)")
+        else:
+            #Verificação se o CPF já está cadastrado
+            existing_cpf = DataMethods.get_user_by_cpf(cpf)
+            if existing_cpf:
+                valid = False
+                error_messages.append("Este CPF já está cadastrado")
+
+            #Validação de CPF usando a função validate_cpf
+            if not validate_cpf(cpf):
+                valid = False
+                error_messages.append("CPF inválido.")
+
+        #Validação da data de nascimento
+        try:
+            datetime.strptime(birth_textfield.value, "%d/%m/%Y")
+        except ValueError:
+            valid = False
+            error_messages.append("Data de nascimento inválida (formato correto: DD/MM/YYYY).")
+
+        #Validação de sexo
+        sex = sex_dropdown.value
+        if not sex:
+            valid = False
+            error_messages.append("Por favor, selecione um sexo.")
+
+        #Validação de telefone
+        phone = phone_textfield.value.strip()
+        if not phone or len(phone) != 11:
+            valid = False
+            error_messages.append("Telefone inválido, deve conter 11 algarismos")
+
+
+        if not valid:
+            page.dialog = AlertDialog(
+                content=Column(controls=[
+                    Text("Erro no cadastro:", size=18, color=colors.RED),
+                    *map(Text, error_messages),
+                ]),
+                actions=[
+                    Row([TextButton("OK", on_click=close_dialog)],alignment=MainAxisAlignment.CENTER)
+                ],
+            )
+            page.dialog.open = True
+            page.update()
+        else:
+            DataMethods.add_users(name_textfield.value,
+                                  email_textfield.value,
+                                  cpf_textfield.value,
+                                  password_textfield.value,
+                                  birth_textfield.value,
+                                  sex_dropdown.value,
+                                  phone_textfield.value,
+                                  blood_type_dropdown.value,
+                                  height_textfield.value,
+                                  weight_textfield.value)
+            page.go("/")
+
+    name_textfield = TextField(label="Nome", width=315, filled=True, bgcolor=colors.WHITE)
+    name_row = Row([name_textfield], alignment=MainAxisAlignment.CENTER)
+
+    email_textfield = TextField(label="Email", width=315, filled=True, bgcolor=colors.WHITE)
+    email_row = Row([email_textfield], alignment=MainAxisAlignment.CENTER)
+
+    password_textfield = TextField(label="Senha", width=315, filled=True, bgcolor=colors.WHITE, password=True, can_reveal_password=True)
+    password_row = Row([password_textfield], alignment=MainAxisAlignment.CENTER)
+
+    confirm_password_textfield = TextField(label="Confirme a senha", width=315, filled=True, bgcolor=colors.WHITE, password=True, can_reveal_password=True)
+    confirm_password_row = Row([confirm_password_textfield], alignment=MainAxisAlignment.CENTER)
+
+    weight_textfield = TextField(label="Peso", width=60, filled=True, bgcolor=colors.WHITE, keyboard_type= KeyboardType.NUMBER, hint_text="kg")
+    weight_row = Row([weight_textfield], alignment=MainAxisAlignment.CENTER)
+
+    height_textfield = TextField(label="Altura", width=70, filled=True, bgcolor=colors.WHITE, keyboard_type= KeyboardType.NUMBER, hint_text="m")
+    height_row = Row([height_textfield], alignment=MainAxisAlignment.CENTER)
+
+    blood_type_dropdown = Dropdown(label="Tipo sanguíneo", width=165, options=[
+        dropdown.Option("A+"),
+        dropdown.Option("A-"),
+        dropdown.Option("B+"),
+        dropdown.Option("B-"),
+        dropdown.Option("AB+"),
+        dropdown.Option("AB-"),
+        dropdown.Option("0+"),
+        dropdown.Option("O-"),
+        dropdown.Option("Não sei"),
+    ])
+    blood_type_dropdown_row = Row([blood_type_dropdown], alignment=MainAxisAlignment.CENTER)
+
+    weight_height_blood_type_row = Row([weight_row, height_row, blood_type_dropdown_row], alignment=MainAxisAlignment.CENTER)
+
+    cpf_textfield = TextField(label="CPF", width=315, filled=True, bgcolor=colors.WHITE, keyboard_type= KeyboardType.NUMBER, hint_text="Apenas números")
+    cpf_row = Row([cpf_textfield], alignment=MainAxisAlignment.CENTER)
+
+    birth_textfield = TextField(label="Data de nascimento", width=190, filled=True, bgcolor=colors.WHITE, hint_text="DD/MM/YYYY")
+    birth_row = Row([birth_textfield], alignment=MainAxisAlignment.CENTER)
+
+    sex_dropdown = Dropdown(label="Sexo", width=115, options=[
+        dropdown.Option("Masculino"),
+        dropdown.Option("Feminino")
+    ])
+    sex_dropdown_row = Row([sex_dropdown], alignment=MainAxisAlignment.CENTER)
+
+    sex_birth_row = Row([birth_row,sex_dropdown_row], alignment=MainAxisAlignment.CENTER)
+
+    phone_textfield = TextField(label="Telefone", width=315, filled=True, bgcolor=colors.WHITE, hint_text="Apenas números")
+    phone_row = Row([phone_textfield], alignment=MainAxisAlignment.CENTER)
+
+    register_button = ElevatedButton(content=Text("Cadastrar", size=15), on_click=send_register, style=ButtonStyle(padding={MaterialState.DEFAULT: 18}), width=140)
+    register_button_row = Row([register_button], alignment=MainAxisAlignment.CENTER)
+
+    def back_to_login(e):
+        page.go("/login")
+
+    back_button = IconButton(
+        icon=icons.ARROW_BACK,
+        on_click=back_to_login,
+        style=ButtonStyle(padding=Padding(top=0, right=0, bottom=0, left=0)),
+    )
+
+    content = Column(
+        scroll=ScrollMode.ALWAYS,
+        controls=[
+            Row(controls=[back_button], alignment=MainAxisAlignment.START),
+            Row(controls=[Text("Cadastro", size=20, weight=FontWeight.W_700)], alignment=MainAxisAlignment.CENTER),
+            name_row,
+            Text("", height=5),
+            email_row,
+            Text("", height=5),
+            password_row,
+            Text("", height=5),
+            confirm_password_row,
+            Text("", height=5),
+            weight_height_blood_type_row,
+            Text("", height=5),
+            cpf_row,
+            Text("", height=5),
+            sex_birth_row,
+            Text("", height=5),
+            phone_row,
+            Text("", height=5),
+            register_button_row
+        ],
+    )
+
     return content
+
+
