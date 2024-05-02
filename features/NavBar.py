f8357c48b7b4ee153c22d13352c6dd142f4090f6
ArthurDeFaria <arthur.faria@aluno.ifsp.edu.br>
2024-05-02 19:59:48 -0300

    Adicionando botão de New Event

--- features/NavBar.py
+++ features/NavBar.py
@@ -1,9 +1,18 @@
 from flet import *
 
 
+def go_newevent(e):
+    e.page.floating_action_button = None
+    e.page.go("/newevent")
+
+
 def on_change(e):
     index = e.control.selected_index
     if index == 0:
+        e.page.floating_action_button = None
         e.page.go("/")
     if index == 1:
+        e.page.floating_action_button = FloatingActionButton(
+            icon=icons.ADD, on_click=go_newevent, bgcolor=colors.RED_400
+        )
         e.page.go("/calendar")
